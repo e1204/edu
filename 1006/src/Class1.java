d2dce63d1b2ae608319cc2b10ebdad527b404b9c
e1234 <91728027+e1204@users.noreply.github.com>
2021-10-06 17:20:38 +0900

    Add files via upload

---
+++ 1006/src/Class1.java
@@ -0,0 +1,180 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		/*int kor = 50;
+		int eng = 70;
+		int math = 95;
+		
+		int score = kor + eng + math;
+		double avg = (double)score / 3;
+		
+		System.out.println("총점 : " + score);
+		System.out.println("평균 : " + avg);*/
+		
+
+		/*int num1 = 11;
+		int num2 = 22;
+		boolean result1, result2;
+		
+		result1 = (num1>1) && (num1<100);
+		result2 = ((num2 % 2) == 0) || ((num2 % 3) == 0);
+		
+		System.out.println(result1);
+		System.out.println(result2);*/
+		
+		/*int num1 = 11;
+		int num2 = 22;
+		boolean result;
+		
+		result = (num1>1) && (num1<100);
+		System.out.println(result);
+		
+		result = ((num2 % 2) == 0) || ((num2 % 3) == 0);
+		System.out.println(result);*/
+		
+		//System.out.println('A');
+		//System.out.println('A' + 'A');
+		
+		/*short num1 = 5;
+		System.out.println(+num1);
+		System.out.println(-num1);
+		
+		short num2 = 7;
+		short num3 = (short)(+num2);
+		short num4 = (short)(-num2);
+		System.out.println(num3);
+		System.out.println(num4);*/
+		
+		/*int num = 5;
+		System.out.print((num++) + " ");
+		System.out.print((num++) + " ");
+		System.out.print(num + "\n");
+		
+		System.out.print((num--) + " ");
+		System.out.print((num--) + " ");
+		System.out.print(num);*/
+		
+		/*byte n1 = 5;   // 00000101
+		byte n2 = 3;   // 00000011
+		byte n3 = -1;  // 11111111
+		
+		byte r1 = (byte)(n1 & n2);   // 00000001
+		byte r2 = (byte)(n1 | n2);   // 00000111
+		byte r3 = (byte)(n1 ^ n2);   // 00000110
+		byte r4 = (byte)(~n3);       // 00000000
+ 		 
+		System.out.println(r1);   // 00000001은 1
+		System.out.println(r2);   // 00000111은 7
+		System.out.println(r3);   // 00000110은 6
+		System.out.println(r4);   // 00000000은 0*/
+		
+		/*int n1 = 5;
+		int n2 = 7;
+		
+		if(n1 < n2) {
+			System.out.println("n1 < n2 is true");
+		}
+		
+		if(n1 == n2) {
+			System.out.println("n1 == n2 is true");
+		}
+		else {
+			System.out.println("n1 == n2 is false");*/
+		
+		/*int num = 60;
+		
+		if(num < 0)
+			System.out.println("0 미만");
+		else if(num < 100)
+			System.out.println("0 이상 100 미만");
+		else
+			System.out.println("100 이상");*/
+		
+		/*int kor = 100;
+		int eng = 100;
+		int math = 90;
+		
+		int score = kor + eng + math;
+		double avg = (double)score / 3;
+		
+		System.out.println(avg);
+		
+		if ((avg >= 90) && (avg <= 100))
+		System.out.println("수입니다");
+		
+		else if ((avg >= 80) && (avg < 90))
+		System.out.println("우입니다");
+		
+		else if (avg >= 70)
+		System.out.println("미입니다");
+		
+		else
+	    System.out.println("가입니다");*/
+		
+		/*int num1;
+		int num2 = 100;
+		int num3 = 10;
+		
+		num1 = (num2 > num3) ? (num2 - num3) : (num3 - num2);
+		
+		System.out.println(num1);*/
+		
+		/*int n = 1;
+		
+		switch(n) {
+			case 1:
+				System.out.println("Simple Java");
+				
+			case 2:
+				System.out.println("Funny java");
+				
+			case 3:
+				System.out.println("Fantastic Java");
+				break;
+			default:
+				System.out.println("The best programming language");
+				
+		}
+		
+		System.out.println("Do you like Java?");*/
+		
+		/*int num = 11;
+		
+		if ((num % 2) == 0)
+			System.out.println("짝수 입니다");
+		else
+			System.out.println("홀수 입니다");*/
+		
+		
+		/*int num1 = 90;
+		int num2 = 100;
+		int num3 = 60;
+		
+		if ((num1 > num2) && (num1 > num3)) {
+			System.out.print("최대값은 ");
+			System.out.print(num1);
+			System.out.print("입니다");
+		}
+		else if((num2 > num1) && (num2 > num3)) {
+			System.out.print("최대값은 ");
+			System.out.print(num2);
+			System.out.print("입니다");
+		}
+		else {
+			System.out.print("최대값은 ");
+			System.out.print(num3);
+			System.out.print("입니다");
+		}*/
+		
+		
+		
+				
+	
+		
+		}
+		
+
+	}
+
