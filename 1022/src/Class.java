333cdca55d3817992c4837a4dc80d8471b704575
e1234 <91728027+e1204@users.noreply.github.com>
2021-10-22 18:34:21 +0900

    Add files via upload

--- 1022/src/Class.java
+++ 1022/src/Class.java
@@ -27,7 +27,6 @@ class Buyer {
 		 * 1.3 장바구니에 구입한 물건을 담는다.(add메서드 호출)
 		 */
 		
-		
 			if(money < p.price) {
 				System.out.println("잔액이 부족하여 " + p + "을/를 살 수 없습니다.");
 			}
@@ -37,8 +36,9 @@ class Buyer {
 				System.out.println(p);
 				return;
 			}
+			
 				
-			}
+	}
 				
 	
 	void add(Product p) {
@@ -50,10 +50,23 @@ class Buyer {
 		 * 1.1.3 새로운 장바구니와 기존의 장바구니를 바꾼다.
 		 * 1.2 물건을 장바구니(cart)에 저장한다 그리고 i의 값을 1 증가시킨다
 		 */
+			
+			for(i = 0; i <= cart.length ; i++) {
+			if(i >= cart.length) {
+				Product[] temp = new Product[cart.length * 2];
+				//기존의 장바구니의 내용을 새로운 배열에 복사한다. -> arraycopy
+				//새로운 장바구니와 기존의 장바구니를 바꾼다. -> cart = temp;
+				System.arraycopy(cart, 0, temp, 0, i);
+				cart = temp;
+				cart[i++] = p;
+				}
+			else
+				return;
+			}	
 		
 		//cart[i] = p;
 		//i++;
-		
+		//cart[i++] = p;
 		
 		
 		
@@ -66,6 +79,22 @@ class Buyer {
 		 * 1.2  장바구니에 담긴 물건들의 가격을 모두 더해서 출력한다
 		 * 1.3  물건을 사고 남은 금액(money) 를 출력한다
 		 */
+		
+		//리스트 뽑아내기
+		/*for(Product e: cart) {
+			
+			System.out.println( e );
+		}
+		 */
+		
+		
+		System.out.println("구입한 물건 : " );
+		System.out.println("사용한 금액 : " );
+		System.out.println("남은 금액 : " + money );
+		
+		
+		
+		
 	} // summary()
 }
 
