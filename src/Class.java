a3fcf078e3620b8068143bb18fd21960511675f8
e1234 <91728027+e1204@users.noreply.github.com>
2021-11-01 17:36:55 +0900

    Add files via upload

---
+++ src/Class.java
@@ -0,0 +1,39 @@
+class Person{
+	
+	private String name;
+	
+	Person(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		Person person = (Person)obj;
+		
+		if(this.name == )
+			return true;
+		return false;
+				
+	}	
+	
+}
+
+
+public class Class {
+	public static void main(String[] args) {
+		
+		Person p1 = new Person("È«±æµ¿");
+		System.out.println(p1.equals(new Person("È«±æµ¿")));
+		System.out.println(p1.equals(new Person("ÃÖ¸íÅÂ")));
+		
+	}
+
+}
+
+
+
+
+
+
+
